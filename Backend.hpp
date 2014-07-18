d9db176197c3e8bea1452782b52b63469b18bf74
Matthew Milano <milano@cs.cornell.edu>
2014-07-18 13:27:46 -0400

    whole lot of re-indenting

--- Backend.hpp
+++ Backend.hpp
@@ -28,19 +28,26 @@ namespace backend {
 		//create/delete object slots
 
 		template<Level L, typename T>
-		Handle<L, T> newHandle(std::unique_ptr<T> r){return newhandle_internal<L>(std::move(r));}
+		Handle<L, T> newHandle(std::unique_ptr<T> r)
+			{return newhandle_internal<L>(std::move(r));}
 
 		template<Level L, typename T>
-		Handle<L, T> newHandle(T r){return newhandle_internal<L>(std::unique_ptr<T>(new T(r)));}
+		Handle<L, T> newHandle(T r)
+			{return newhandle_internal<L>
+					(std::unique_ptr<T>(new T(r)));}
 
 		template<Level L, typename T>
-		Handle<L, T> newHandle(T* r = nullptr){return newhandle_internal<L>(std::unique_ptr<T>(r));}
+		Handle<L, T> newHandle(T* r = nullptr)
+			{return newhandle_internal<L>
+					(std::unique_ptr<T>(r));}
 
 		template<Level L, typename T>
-		std::unique_ptr<T> del(Handle<L, T>& hndl) {return del_internal<L>(hndl);}
+		std::unique_ptr<T> del(Handle<L, T>& hndl) 
+			{return del_internal<L>(hndl);}
 
 		template<Level Lnew, typename T>
-		Handle<Lnew, T> newConsistency(TypedHandle<T> &old){ return Handle<Lnew, T>(old.hi());}
+		Handle<Lnew, T> newConsistency(TypedHandle<T> &old)
+			{ return Handle<Lnew, T>(old.hi());}
 
 
 		//KVstore-style interface
@@ -48,28 +55,44 @@ namespace backend {
 		template<Level L, Level L_effective = L, typename T>
 		T& get(Handle<L, T> &hndl) {return hndl.hi();}
 
-		template<Level L, Level L_effective = L, typename T>
-		void give(Handle<L, T> &hndl, std::unique_ptr<T> obj) {hndl.hi() = std::move(obj);}
+		template<Level L_effective, typename T>
+		T& get(TypedHandle<T> &hndl) {return hndl.hi();}
 
 		template<Level L, Level L_effective = L, typename T>
-		void give(Handle<L, T> &hndl, T* obj) {hndl.hi() = std::unique_ptr<T>(obj);}
+		void give(Handle<L, T> &hndl, std::unique_ptr<T> obj) 
+			{hndl.hi() = std::move(obj);}
+
+		template<Level L, Level L_effective = L, typename T>
+		void give(Handle<L, T> &hndl, T* obj) 
+			{hndl.hi() = std::unique_ptr<T>(obj);}
 		
 		template<Level L, Level L_effective = L, typename T>
-		std::unique_ptr<T> take(Handle<L, T>& hndl){ return hndl.hi();}
+		std::unique_ptr<T> take(Handle<L, T>& hndl)
+			{ return hndl.hi();}
 
 		//commutative operations
 
 		template<Level L, Level L_effective = L, typename T>
-		void incr_op(Handle<L, T> &h) {h.hi().stored_obj->operator++();}
+		void incr_op(Handle<L, T> &h) 
+			{(*(h.hi().stored_obj))++;}
 
 		template<Level L, Level L_effective = L, typename T>
 		void incr(Handle<L, T> &h) {h.hi().stored_obj->incr();}
 
-		template<Level L, Level L_effective = L, typename T, typename... A>
-		void add(Handle<L, T> &h, A... args) {h.hi().stored_obj->add(args...);}
+		template<Level L, Level L_effective = L,
+			 typename T, typename... A>
+		void add(Handle<L, T> &h, A... args) 
+			{h.hi().stored_obj->add(args...);}
 
-		template<Level L, Level L_effective = L, typename T, typename F, typename... A>
-		void add_f(Handle<L, T> &h, F addfun, A... args) {F(*(h.hi().stored_obj), args...);}
+		template<Level L, Level L_effective = L, 
+			 typename T, typename... A>
+		void size(Handle<L, T> &h, A... args) 
+			{h.hi().stored_obj->size(args...);}
+
+		template<Level L, Level L_effective = L, 
+			 typename T, typename F, typename... A>
+		void add_f(Handle<L, T> &h, F addfun, A... args) 
+			{F(*(h.hi().stored_obj), args...);}
 
 		//constructors and destructor
 
