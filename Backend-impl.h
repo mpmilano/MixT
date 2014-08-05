2201cbda27bcc56a0155e7bc7d1930b1586989f9
Matthew Milano <milano@cs.cornell.edu>
2014-08-05 00:20:10 -0400

    more refactor

--- Backend-impl.h
+++ Backend-impl.h
@@ -143,18 +143,3 @@ public:
 	template<Client_Id>
 	friend class Client;
 };
-
-template<Client_Id id, Level L, HandleAccess HA, typename T>	
-auto newhandle_internal(std::unique_ptr<T> r) {
-	return Handle<id,L,HA,T>
-		(HandleImpl<T>::constructAndPlace(*this,std::move(r)));
-}
-
-template<Client_Id id, Level L, HandleAccess HA, typename T>
-auto del_internal(Handle<id,L,HA,T> &hndl_i){
-	auto &hndl = hndl_i.hi(); 
-	std::unique_ptr<T> ret = hndl;
-	assert(hndls[hndl.id]->id == hndl.id);
-	hndls[hndl.id].first.reset(nullptr);
-	return ret;
-}
--- Backend.hpp
+++ Backend.hpp
@@ -103,6 +103,23 @@ namespace backend{
 			const_cast<DataStore*>(this)->mut.unlock();
 		}
 
+
+		template<Client_Id id, Level L, HandleAccess HA, typename T>	
+		auto newhandle_internal(std::unique_ptr<T> r) {
+			return Handle<id,L,HA,T>
+				(HandleImpl<T>::constructAndPlace(*this,std::move(r)));
+		}
+
+		template<Client_Id id, Level L, HandleAccess HA, typename T>
+		auto del_internal(Handle<id,L,HA,T> &hndl_i){
+			auto &hndl = hndl_i.hi(); 
+			std::unique_ptr<T> ret = hndl;
+			assert(hndls[hndl.id]->id == hndl.id);
+			hndls[hndl.id].first.reset(nullptr);
+			return ret;
+		}
+
+
 	};
 
 }
