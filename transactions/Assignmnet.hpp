af87f3d705afeda4b3e51ab13e34e6059b260eee
Matthew Milano <milano@cs.cornell.edu>
2015-06-25 22:35:58 -0400

    assignment

---
+++ transactions/Assignmnet.hpp
@@ -0,0 +1,35 @@
+#pragma once
+#include "ConStatement.hpp"
+#include "BaseCS.hpp"
+#include "ConExpr.hpp"
+#include "Seq.hpp"
+#include "../Backend.hpp"
+#include <iostream>
+
+using namespace backend;
+
+template<typename Expr>
+class Assignment : ConStatement<get_level<Expr>::value> {
+	static constexpr Level level = get_level<Expr>::value;
+	static_assert(is_ConExpr<Expr>::value,"must assign result of expression only!");
+		
+public:
+
+	const std::function<void ()> thunk;
+	
+	template<Client_Id id, Level l, HandleAccess HA, typename T>
+	Assignment(DataStore::Handle<id,l,HA,T> h, Expr e):thunk([=](){return h.clone().put(e);}){
+		static_assert(canWrite(HA),"Error: needs writeable handle!");
+		static_assert(l == Level::causal || level == Level::strong,"Error: flow violation");
+	}
+
+	void operator()(){
+		thunk();
+	}
+
+};
+
+template<Level l, Client_Id id, HandleAccess HA, typename T, typename Expr>
+Assignment<Expr> operator<<(DataStore::Handle<id,l,HA,T> &sink, Expr source) {
+	return Assignment<Expr>(sink,source);
+}
