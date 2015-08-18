e409e47f857cab97778afa339be945682a455c9b
Matthew Milano <milano@cs.cornell.edu>
2015-08-18 18:01:30 -0400

    tempbuilder phase1 debug

--- transactions/TempBuilder.hpp
+++ transactions/TempBuilder.hpp
@@ -33,15 +33,16 @@ struct DeclarationScope : public ConStatement<l>{
 	
 };
 
-template<unsigned long long id, Level l, typename T>
+template<unsigned long long id, Level l, typename T, typename CS>
 auto isValid_desugar(Temporary<id,l,T> const * const gt, const CS &cs){
 	return make_if(
 		make_isValid(
-			RefTemporary<ID,get_level<Temp>::value, typename extract_type<Temp>::type,Temp>{*gt}),
-		, cs, std::tuple<>());
+			RefTemporary<id,l,T,Temporary<id,l,T> >{*gt})
+		, cs, std::make_tuple());
 }
 
-auto isValid_desugar(std::nullptr_t, const CS &cs){
+template<typename CS>
+auto isValid_desugar(std::nullptr_t*, const CS &cs){
 	return cs;
 }
 
@@ -70,14 +71,14 @@ struct MutDeclarationScope : public DeclarationScope<ID,CS,l,Temp>{
 };
 
 
-template<unsigned long long ID, typename CS, Level l>
-auto find_usage(const DeclarationScope<ID,CS,l>&){
+template<unsigned long long ID, typename CS, Level l, typename Temp >
+auto find_usage(const DeclarationScope<ID,CS,l,Temp>&){
 	//we have been shadowed!
 	return nullptr;
 }
 
-template<unsigned long long ID, unsigned long long ID2, typename CS, Level l, restrict(ID != ID2)>
-auto find_usage(const DeclarationScope<ID2,CS,l>& ds){
+template<unsigned long long ID, unsigned long long ID2, typename CS, Level l, typename temp, restrict(ID != ID2)>
+auto find_usage(const DeclarationScope<ID2,CS,l,temp>& ds){
 	return fold(ds.cs,
 				[](const auto &e, const auto &acc){
 					if (!acc){
@@ -88,8 +89,8 @@ auto find_usage(const DeclarationScope<ID2,CS,l>& ds){
 				, nullptr);
 }
 
-template<unsigned long long ID,typename CS, Level l>
-std::ostream & operator<<(std::ostream &os, const DeclarationScope<ID,CS,l> &t){
+template<unsigned long long ID,typename CS, Level l, typename temp>
+std::ostream & operator<<(std::ostream &os, const DeclarationScope<ID,CS,l,temp> &t){
 	assert(t.gt);
 	os << t.name << "<" << levelStr<l>() <<"> = " << t.gt->gets;
 	fold(t.cs,[&os](const auto &e, int) -> int
@@ -103,7 +104,7 @@ struct _impl_pick_new_type;
 template<unsigned long long ID, Level l, typename T>
 struct _impl_pick_new_type<Temporary<ID,l,T>,Temporary<ID,l,T> > {
 	using type = Temporary<ID,l,T>;
-}
+};
 
 template<unsigned long long ID, Level l, typename T>
 struct _impl_pick_new_type<MutableTemporary<ID,l,T>,MutableTemporary<ID,l,T> > {
@@ -120,6 +121,7 @@ struct _impl_pick_new_type<std::nullptr_t, T > {
 	using type = T;
 };
 
+template<>
 struct _impl_pick_new_type<std::nullptr_t, std::nullptr_t > {
 	using type = std::nullptr_t;
 };
@@ -129,7 +131,7 @@ struct MutableDeclarationBuilder {
 	const PrevBuilder prevBuilder;
 	const MutDeclarationScope<ID,CS,l,Temp> this_decl;
 
-	DeclarationBuilder(const PrevBuilder &pb, const MutDeclarationScope<ID,CS,l,Temp> &d)
+	MutableDeclarationBuilder(const PrevBuilder &pb, const MutDeclarationScope<ID,CS,l,Temp> &d)
 		:prevBuilder(pb),this_decl(d){}
 
 	template<typename T>
@@ -139,9 +141,9 @@ struct MutableDeclarationBuilder {
 		auto new_cs = std::tuple_cat(this_decl.cs,std::tuple<T>(t));
 
 		typedef typename _impl_pick_new_type<
-			std::decay<*decltype(find_usage<ID>(t))>::type,
-			std::decay<*this_decl.gt>::type
-			> found_type;
+			typename std::decay<decltype(*find_usage<ID>(t))>::type,
+			typename std::decay<decltype(*this_decl.gt)>::type
+			>::type found_type;
 
 		constexpr Level new_level = get_level<found_type>::value;
 		MutDeclarationScope<ID, decltype(new_cs),new_level,found_type>
@@ -150,7 +152,7 @@ struct MutableDeclarationBuilder {
 					  this_decl.gt :
 					  make_shared<found_type>(find_usage<ID>(t))),
 					 new_cs);
-		MutDeclarationBuilder<PrevBuilder, ID, decltype(new_cs), new_level, contains_temporary<ID,T>::value || oldb, found_type>
+		::MutableDeclarationBuilder<PrevBuilder, ID, decltype(new_cs), new_level, contains_temporary<ID,T>::value || oldb, found_type>
 			r(prevBuilder,new_decl);
 		return r;
 	}
@@ -161,7 +163,7 @@ struct ImmutableDeclarationBuilder {
 	const PrevBuilder prevBuilder;
 	const ImmutDeclarationScope<ID,CS,l,Temp> this_decl;
 
-	DeclarationBuilder(const PrevBuilder &pb, const ImmutDeclarationScope<ID,CS,l,Temp> &d)
+	ImmutableDeclarationBuilder(const PrevBuilder &pb, const ImmutDeclarationScope<ID,CS,l,Temp> &d)
 		:prevBuilder(pb),this_decl(d){}
 
 	template<typename T>
@@ -171,9 +173,10 @@ struct ImmutableDeclarationBuilder {
 		auto new_cs = std::tuple_cat(this_decl.cs,std::tuple<T>(t));
 
 		typedef typename _impl_pick_new_type<
-			std::decay<*decltype(find_usage<ID>(t))>::type,
-			std::decay<*this_decl.gt>::type
-			> found_type;
+			typename std::decay<decltype(*find_usage<ID>(t))>::type,
+			typename std::decay<decltype(*this_decl.gt)>::type
+			>::type found_type;
+
 
 		constexpr Level new_level = get_level<found_type>::value;
 		ImmutDeclarationScope<ID, decltype(new_cs),new_level,found_type>
@@ -182,11 +185,11 @@ struct ImmutableDeclarationBuilder {
 					  this_decl.gt :
 					  make_shared<found_type>(find_usage<ID>(t))),
 					 new_cs);
-		ImmutDeclarationBuilder<PrevBuilder, ID, decltype(new_cs), new_level, contains_temporary<ID,T>::value || oldb, found_type>
+		::ImmutableDeclarationBuilder<PrevBuilder, ID, decltype(new_cs), new_level, contains_temporary<ID,T>::value || oldb, found_type>
 			r(prevBuilder,new_decl);
 		return r;
 	}
-}
+};
 
 template<unsigned long long ID>
 struct MutVarScopeBegin {
@@ -201,15 +204,15 @@ struct ImmutVarScopeBegin {
 
 template<typename PrevBuilder, unsigned long long ID>
 auto append(const PrevBuilder &pb, const MutVarScopeBegin<ID> &vsb){
-	MutDeclarationScope<ID,std::tuple<>,Level::strong,'' > dcl(vsb.name,nullptr,std::tuple<>());
-	MutDeclarationBuilder<PrevBuilder, ID, std::tuple<>,Level::strong, false > db(pb,dcl);
+	MutDeclarationScope<ID,std::tuple<>,Level::strong,std::nullptr_t > dcl(vsb.name,nullptr,std::tuple<>());
+	MutableDeclarationBuilder<PrevBuilder, ID, std::tuple<>,Level::strong, false, std::nullptr_t > db(pb,dcl);
 	return db;
 }
 
 template<typename PrevBuilder, unsigned long long ID>
 auto append(const PrevBuilder &pb, const ImmutVarScopeBegin<ID> &vsb){
 	ImmutDeclarationScope<ID,std::tuple<>,Level::strong,std::nullptr_t> dcl(vsb.name,nullptr,std::tuple<>());
-	ImmutDeclarationBuilder<PrevBuilder, ID, std::tuple<>,Level::strong, false, std::nullptr_t > db(pb,dcl);
+	ImmutableDeclarationBuilder<PrevBuilder, ID, std::tuple<>,Level::strong, false, std::nullptr_t > db(pb,dcl);
 	return db;
 }
 
@@ -222,13 +225,13 @@ const auto& end_var_scope() {
 }
 
 template<typename PrevBuilder, unsigned long long ID, typename CS, Level l, bool b, typename temp>
-auto append(const MutDeclarationBuilder<PrevBuilder, ID, CS, l, b,temp>  &pb, const VarScopeEnd&){
+auto append(const MutableDeclarationBuilder<PrevBuilder, ID, CS, l, b,temp>  &pb, const VarScopeEnd&){
 	static_assert(b,"Error: must use all temporaries you define in a transaction.");
 	return append(pb.prevBuilder, pb.this_decl);
 }
 
-template<typename PrevBuilder, unsigned long long ID, typename CS, Level l, bool b, template temp>
-auto append(const ImmutDeclarationBuilder<PrevBuilder, ID, CS, l, b, temp>  &pb, const VarScopeEnd&){
+template<typename PrevBuilder, unsigned long long ID, typename CS, Level l, bool b, typename temp>
+auto append(const ImmutableDeclarationBuilder<PrevBuilder, ID, CS, l, b, temp>  &pb, const VarScopeEnd&){
 	static_assert(b,"Error: must use all temporaries you define in a transaction.");
 	return append(pb.prevBuilder, pb.this_decl);
 }
