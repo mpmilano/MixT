562bd080cdb1a50b534b792e6e392895f585a2a9
Matthew Milano <milano@cs.cornell.edu>
2015-08-31 23:51:02 -0400

    inching ever closer

--- transactions/Transaction.hpp
+++ transactions/Transaction.hpp
@@ -28,19 +28,29 @@ struct Transaction{
 				//TODO: encode that semantics in the type system.
 
 
-				std::map<GDataStore*,std::unique_ptr<TransactionContext> > tc;
+				std::map<Level,
+						 std::map< GDataStore*,
+								   std::unique_ptr<TransactionContext> > > tc;
 
 				std::map<GeneralRemoteObject*, TransactionContext*> old_ctx;
+
+				auto tc_without = [&](auto* sto){
+					return
+					(tc.count(sto->level) == 0) ||
+					tc.at(sto->level).count(sto) == 0;
+				};
 
 				foreach(s.curr, [&](const auto &e){
 						foreach(e.handles(),[&](const auto &h){
 								auto *sto = &h._ro->store();
 								auto *ro = h._ro.get();
 								old_ctx[ro] = ro->currentTransactionContext();
-								if (tc.count(sto) == 0){
-									tc.emplace(sto, sto->begin_transaction());
+								if (tc_without(sto) == 0){
+									tc[sto->level]
+										.emplace(sto, sto->begin_transaction());
 								}
-								ro->setTransactionContext(tc.at(sto).get());
+								ro->setTransactionContext(
+									tc.at(sto->level).at(sto).get());
 							});});
 				
 				Store cache;
@@ -51,11 +61,22 @@ struct Transaction{
 				for (auto &p : old_ctx){
 					p.first->setTransactionContext(p.second);
 				}
-				
-				//todo: end transaction
 
-				//todo: errors
-				return true;
+				//todo: REPLICATE THIS COMMIT
+				bool ret = true;
+				for (auto &p : tc.at(Level::strong)){
+					ret = ret && p.second->commit();
+				}
+
+				//causal commits definitionally can't fail!
+				if (ret){
+					for (auto &p : tc.at(Level::causal)){
+					ret = ret && p.second->commit();
+					}
+				}
+
+				//TODO: exception here instead of boolean?
+				return ret;
 				
 			}),
 		print([s](std::ostream &os) -> std::ostream& {
