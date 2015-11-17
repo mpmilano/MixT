b64ba1dc305701d56326d692f7d8ce2b7623e4c4
Matthew Milano <milano@cs.cornell.edu>
2015-11-16 23:09:20 -0500

    eucalyptus management files

---
+++ euc/vm-actions.sh
@@ -0,0 +1,20 @@
+#echo hello `whoami`
+#echo "I have id $1"
+mkdir /mnt/gentoo 2>/dev/null
+mount /dev/vdb /mnt/gentoo 2>/dev/null
+cp /home/ubuntu/as-gentoo.sh /mnt/gentoo 
+cd /mnt/gentoo/
+mount --rbind /dev dev 2>/dev/null
+rm /dev/shm 2>/dev/null
+mkdir /dev/shm 2>/dev/null
+mount -t tmpfs shm /dev/shm 2>/dev/null
+mount --make-rslave dev 2>/dev/null
+mount --rbind /sys/ sys 2>/dev/null
+mount -t proc none proc 2>/dev/null
+mount --make-rslave sys 2>/dev/null
+cp -L /etc/resolv.conf etc/ 2>/dev/null
+cd /mnt/
+#echo "done so far" `pwd`
+chroot gentoo /bin/su -c "/bin/bash /as-gentoo.sh $1" research
+#chroot gentoo /bin/su -c "/usr/bin/emerge libunwind" root
+
