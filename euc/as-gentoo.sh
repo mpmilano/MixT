7496faa09c9ae1f2e65970ca19374c1edcf676e7
Matthew Milano <milano@cs.cornell.edu>
2015-11-17 01:37:36 -0500

    small tweaks

--- euc/as-gentoo.sh
+++ euc/as-gentoo.sh
@@ -5,6 +5,7 @@ git checkout pg_env.sh
 git pull
 source pg_env.sh
 killall -9 vm
+rm /tmp/Myria*
 make clean
 causalGroup="$1" make -j4 vm
 sleep 10
--- transactions/vm_main.cpp
+++ transactions/vm_main.cpp
@@ -122,7 +122,7 @@ int main(){
 		int personal_generator = 0;
 		int light_generator = 0;
 		int heavy_generator = 0;
-		for (int i = 0; i < 10; ++i){
+		for (int i = 0; i < 20; ++i){
 			writemix(light_generator,4,10,light);
 			writemix(heavy_generator,1,5,heavy);
 			writemix(personal_generator,5,10,personal);
