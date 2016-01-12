383257f9afa55be9018ba17c674b42f4b0150467
Matthew Milano <milano@cs.cornell.edu>
2016-01-11 23:13:23 -0500

    checkpoint; need to replay entire transaction in case of failure, not just commit statement!

--- transactions/mtl/Transaction.hpp
+++ transactions/mtl/Transaction.hpp
@@ -59,20 +59,12 @@ namespace myria { namespace mtl {
 						//By which we mean if they need to handle in-a-transaction
 						//in a special way, they do that for themselves.
 
-						//note: eiger only support read-only or write-only
-						//transactions.  which for the moment means we will
-						//mark as read/write only the first time we read/write,
-						//and just barf if we wind up mixing.
-
-						//TODO: encode that semantics in the type system.
-
-
 						std::map<Level,
 								 std::map< GDataStore*,
 										   std::unique_ptr<TransactionContext> > > tc;
 
-						std::map<GeneralRemoteObject<Level::strong>*, TransactionContext*> old_ctx_s;
-						std::map<GeneralRemoteObject<Level::causal>*, TransactionContext*> old_ctx_c;
+						std::set<std::shared_ptr<GeneralRemoteObject<Level::causal> > > collected_objs_c;
+						std::set<std::shared_ptr<GeneralRemoteObject<Level::strong> > > collected_objs_s;
 
 						auto tc_without = [&](auto* sto){
 							return
@@ -80,104 +72,116 @@ namespace myria { namespace mtl {
 							tc.at(sto->level).count(sto) == 0;
 						};
 
-						{
-							bool any = false;
+						mutils::foreach(s.curr, [&](const auto &e){
+								mutils::foreach(e.handles(),[&](const auto &h){
+										Transaction::collected_objs_insert(collected_objs_s, collected_objs_c,h._ro);
+									});});
+						
+						auto& trk = tracker::Tracker::global_tracker();
 
+													
+						auto assign_transaction_start = [&](
+							std::map<GeneralRemoteObject<Level::strong>*, TransactionContext*> *old_ctx_s,
+							std::map<GeneralRemoteObject<Level::causal>*, TransactionContext*> *old_ctx_c, auto &_ro){
+							auto *sto = &_ro->store();
+							auto *ro = _ro.get();
+							assign_current_context(*old_ctx_s,*old_ctx_c,ro);
+							if (tc_without(sto)){
+								tc[sto->level]
+									.emplace(sto, sto->begin_transaction(trk));
+							}
+							assert(!tc_without(sto));
+							assert(ro->currentTransactionContext() == nullptr);
+							assert(&ro->store() == sto);
+							assert(sto == &tc.at(sto->level).at(sto)->store());
+							ro->setTransactionContext(
+								tc.at(sto->level).at(sto).get());
+						};
+
+						{
+						
+							//strong execution begins
+							
 							//TODO: it'd probably be better to keep the association of RemoteObject
 							//to TransactionContext here, and to pass the transactionContext
 							//in when performing operations inside a transaction
-				
+
+							
 							//this loop finds all stores, calls begin_transaction on them exactly once,
 							//and sets their participating RemoteObjects' current transaction pointers.
 
-							std::set<std::shared_ptr<GeneralRemoteObject<Level::strong> > > collected_objs_s;
-							std::set<std::shared_ptr<GeneralRemoteObject<Level::causal> > > collected_objs_c;
-							mutils::foreach(s.curr, [&](const auto &e){
-									mutils::foreach(e.handles(),[&](const auto &h){
-											any = true;
-											Transaction::collected_objs_insert(collected_objs_s, collected_objs_c,h._ro);
-										});});
+							std::map<GeneralRemoteObject<Level::strong>*, TransactionContext*> old_ctx_s;
 
-							auto& trk = tracker::Tracker::global_tracker();
-							auto collected_objs_proc = [&](auto &_ro){
-								auto *sto = &_ro->store();
-								auto *ro = _ro.get();
-								assign_current_context(old_ctx_s,old_ctx_c,ro);
-								if (tc_without(sto)){
-									tc[sto->level]
-										.emplace(sto, sto->begin_transaction(trk));
-								}
-								assert(!tc_without(sto));
-								assert(ro->currentTransactionContext() == nullptr);
-								assert(&ro->store() == sto);
-								assert(sto == &tc.at(sto->level).at(sto)->store());
-								ro->setTransactionContext(
-									tc.at(sto->level).at(sto).get());
-							};
 							for (auto &_ro : collected_objs_s){
-								collected_objs_proc(_ro);
+								assign_transaction_start(&old_ctx_s, nullptr,_ro);
 							}
-							for (auto &ro : collected_objs_c){
-								collected_objs_proc(ro);
-							}
-				
-							assert(any && "no handles traversed");
-						}
-				
-						StrongCache caches;
-						StrongStore stores;
-						set_context(caches,context::t::unknown);
-						call_all_strong(caches,stores,s.curr);
-						CausalCache &cachec = *((CausalCache*) (&caches));
-						CausalStore &storec = *((CausalStore*) (&stores));
-						call_all_causal(cachec,storec,s.curr);
-
-						//restore the old transaction pointers
-						for (auto &p : old_ctx_s){
-							p.first->setTransactionContext(p.second);
-							assert(p.first->currentTransactionContext() == nullptr);
-						}
-						for (auto &p : old_ctx_c){
-							p.first->setTransactionContext(p.second);
-							assert(p.first->currentTransactionContext() == nullptr);
-						}
-
-
-						//todo: REPLICATE THIS COMMIT
-						bool ret = true;
-						{
-							bool any = false;
-							if (tc.count(Level::strong) != 0){
-								any = true;
-								for (auto &p : tc.at(Level::strong)){
-									ret = ret && p.second->full_commit();
-								}
-							}
-					
-							//causal commits definitionally can't fail!
-							if (ret && tc.count(Level::causal) != 0){
-								any = true;
-								for (auto &p : tc.at(Level::causal)){
-									using namespace std::chrono;
-									auto backoff = 2us;
-									while (true){
-										try{ 
-											ret = ret && p.second->full_commit();
-											assert(ret);
-											break;
-										}
-										catch (...){
-											std::this_thread::sleep_for(backoff);
-											backoff *= 2;
-											//we really don't want this to fail guys.
-										}
+							
+							StrongCache caches;
+							StrongStore stores;
+							set_context(caches,context::t::unknown);
+							call_all_strong(caches,stores,s.curr);
+							
+							//todo: REPLICATE THIS COMMIT
+							bool ret = true;
+							{
+								if (tc.count(Level::strong) != 0){
+									for (auto &p : tc.at(Level::strong)){
+										ret = ret && p.second->full_commit();
 									}
 								}
 							}
-					
 
-							assert(any);
+							//restore the old transaction pointers
+							
+							for (auto &p : old_ctx_s){
+								p.first->setTransactionContext(p.second);
+								assert(p.first->currentTransactionContext() == nullptr);
+							}
 						}
+
+						do {
+								//causal execution.  it can't fail.
+
+							std::map<GeneralRemoteObject<Level::causal>*, TransactionContext*> old_ctx_c;
+
+							mutils::AtScopeEnd ase{[&](){
+									//restore old pointers at end; ready for retry
+									for (auto &p : old_ctx_c){
+										p.first->setTransactionContext(p.second);
+										assert(p.first->currentTransactionContext() == nullptr);
+									}
+								}};
+							
+							for (auto &ro : collected_objs_c){
+								assign_transaction_start(nullptr, &old_ctx_c, ro);
+							}
+							CausalCache &cachec = *((CausalCache*) (&caches));
+							CausalStore &storec = *((CausalStore*) (&stores));
+							call_all_causal(cachec,storec,s.curr);
+							
+							
+							//causal commits definitionally can't fail!
+							//so we'll just try the causal transaction again if it does!
+							if (ret && tc.count(Level::causal) != 0){
+								for (auto &p : tc.at(Level::causal)){
+									using namespace std::chrono;
+									auto backoff = 2us;
+									
+									try{ 
+										ret = ret && p.second->full_commit();
+										assert(ret);
+										break;
+									}
+									catch (...){
+										std::this_thread::sleep_for(backoff);
+										backoff *= 2;
+										//we really don't want this to fail guys.
+									}
+									
+								}
+							}
+						} while (true);
+						
 						assert(ret);
 						//TODO: exception here instead of boolean?
 						return ret;
