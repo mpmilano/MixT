c373a4187ee3b0a8c8d55b379153b6a64b8f5685
Matthew Milano <milano@cs.cornell.edu>
2016-01-20 21:16:21 -0500

    first draft of code for new StoreMap

--- transactions/mtl/Store.hpp
+++ transactions/mtl/Store.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <list>
 #include "utils.hpp"
+#include "HeteroMap.hpp"
 
 namespace myria { namespace mtl {
 
@@ -16,190 +17,79 @@ namespace myria { namespace mtl {
 		constexpr bool is_store(StoreType st){
 			return st == StoreType::CausalStore || st == StoreType::StrongStore;
 		}
-
-
-		static std::map<int, void*> store_lost_and_found_map;
-
-		template<StoreType semantic_switch>
-		struct StoreMap {
+		
+		struct StoreMap_base {
 		private:
-			struct internal_store{
-				static int new_id(int reset_id = -1){
-					static int max_id_counter = 0;
-					static std::list<int> free_map_ids;
-					if (reset_id != -1){
-						//reset things
-						free_map_ids.push_back(reset_id);
-						return -1;
-					}
-					else 
-						return
-							(free_map_ids.empty() ? 
-							 //take current max_id_counter, increment it
-							 max_id_counter++
-							 : 
-							 //grab an id from the list
-							 [&](){auto ret = free_map_ids.front();
-								free_map_ids.pop_front();
-								return ret;}());
-				}
-				
-				template<typename T>
-				static std::map<int,std::unique_ptr<T> >& type_specific_store(int id, bool del){
-					static std::map<int, std::map<int,std::unique_ptr<T> > > in_use_maps;
-					if (del) {
-						new_id(id); //release this id
-						in_use_maps[id].clear();
-					}
-					return in_use_maps[id];
-				}
-
-				const int store_id{new_id()};
-				struct internal_store_shared{
-					template<typename T>
-					static const auto& at(int store_id, int id, T*) {
-						return internal_store::type_specific_store<T>(store_id,false).at(id);
-					}
-					
-					template<typename T>
-					static auto& mut(int store_id, int id, T*){
-						return internal_store::type_specific_store<T>(store_id,false)[id];
-					}
-				};
-				std::set<int> contains_set; //keys that we can map
-
-				template<typename T>
-				const std::unique_ptr<T>& at(int i) const{
-					T* dummy{nullptr};
-					assert(contains(i));
-					return internal_store_shared::at(store_id, i,dummy);
-				}
-
-				template<typename T>
-				auto &mut(int id){
-					T* dummy{nullptr};
-					return internal_store_shared::mut(store_id,id,dummy);
-				}
-
-				bool contains(int i) const{
-					return (contains_set.count(i) > 0);
-				}
-
-				int count(int i) const {
-					return contains_set.count(i);
-				}
-
-				virtual ~internal_store(){
-					new_id(store_id);
-				}
-				
-			};
-
-			internal_store store_impl;
-
-
-			template<StoreType ss,restrict(ss != semantic_switch)>
-			explicit StoreMap(StoreMap<ss> &&sm)
-				:looping(sm.looping),
-				 above(sm.above),
-				 store_impl(std::move(sm.store_impl)),
-				 valid_store(sm.valid_store){
-				sm.valid_store = false;
-			}
-	
-		public:
-	
-			static std::map<int, StoreMap*>& lost_and_found() {
-				return *((std::map<int, StoreMap*>*) &store_lost_and_found_map);
-			};
-
-			bool looping = false;
-			StoreMap const * const above{nullptr};
-			void in_loop() {
-				assert(is_store(semantic_switch));
-				//yeah, no nested while-loops for now.
-				//will be obvious later.
-				assert(!looping);
-				looping = true;
-			}
-			void out_of_loop() {
-				assert(looping);
-				assert(is_store(semantic_switch));
-				looping = false;
-			}
+			StoreMap_base const * const above{nullptr};
+			HeteroMap<int> store_impl;
 	
 			bool valid_store = true;
+			
+		protected:
+
+			explicit StoreMap_base(const StoreMap_base& ss, bool ThisIsNotACopyConstructor):above(ss){}
+
+			StoreMap_base(){}
+		public:
+			StoreMap_base(const StoreMap_base&) = delete;
+			
 			bool contains(int i) const{
 				return store_impl.contains(i) ||
 					(above && (above->contains(i)));
 			}
 
-			StoreMap(){}
-
-			template<StoreType ss,restrict(ss != semantic_switch)>
-			explicit StoreMap(const StoreMap<ss> &sm):above((StoreMap const * const) &sm){}
-
-			StoreMap(const StoreMap&) = delete;
-
-#define dbg_store_prnt(y,x) //if (is_store(semantic_switch)) std::cout << y << " this value (" << i << "): " << x << std::endl;
-#define dbg_store_prnt2 //if (is_store(semantic_switch)) std::cout << "Getting this value (" << i << "): " << *ret << std::endl;
-	
 			template<typename T>
 			void insert(int i, const T &item) {
 				assert(valid_store);
-				if (!looping) assert(!contains(i));
+				assert(!contains(i));
 				store_impl.template mut<T>(i).reset(mutils::heap_copy(item).release());
-				lost_and_found()[i] = this;
 				assert(contains(i));
-				dbg_store_prnt("Storing",item)
-					}
+			}
 
 			template<typename T, typename... Args>
 			void emplace_ovrt(int i, Args && ... args){
 				assert(valid_store);
 				store_impl.template mut<T>(i).reset(new T(std::forward<Args>(args)...));
-				lost_and_found()[i] = this;
 				assert(contains(i));
-				dbg_store_prnt("Emplacing",*((T*) store_impl.at<T>(i).get()))
-					}
+			}
 
 			template<typename T, typename... Args>
 			void emplace(int i, Args && ... args){
 				assert(valid_store);
-				if (!looping) assert(!contains(i));
+				assert(!contains(i));
 				emplace_ovrt<T>(i,std::forward<Args>(args)...);
 			}
-
-		private:
-			auto get_assert(int i) const {
+			
+			template<typename T>
+			const T& get(int i) const{
+				//get_assert
 				if (!contains(i)) {
 					std::cerr << "trying to find something of id " << i
 							  << " in a " << (is_store(semantic_switch) ? "store" : "cache" ) <<std::endl;
 					std::cerr << "we have that here : "	<< lost_and_found()[i] <<std::endl;
 					throw StoreMiss{};
-				}	
-			}
-			
-		public:
+				}
 
-			
-			template<typename T>
-			const T& get(int i) const{
-				get_assert(i);
+				//actual method
 				if (!store_impl.contains(i)) {
 					return above->template get<T>(i);
 				}
 				T* ret = (store_impl.template at<T>(i).get());
 				assert(ret);
-				dbg_store_prnt2;
 				return *ret;
 			}
-			
+		
 			virtual ~StoreMap() {
 				valid_store = false;
 			}
 
-			friend struct Transaction;
+		};
+
+		template<StoreType st>
+		struct StoreMap : public StoreMap_base{
+			template<StoreType ss,restrict(ss != semantic_switch)>
+			explicit StoreMap(const StoreMap<ss> &sm):StoreMap_base(sm,true){}
+			StoreMap(){}
 		};
 
 		using StrongStore = StoreMap<StoreType::StrongStore>;
---
+++ transactions/mutils/HeteroMap.hpp
@@ -0,0 +1,74 @@
+#pragma once
+#include <map>
+#include <pair>
+#include <set>
+
+namespace myria { namespace mutils {
+
+		template<typename Key>
+		struct HeteroMap {
+		private:
+
+			using type_id = int;
+			using destructor = std::function<void ()>;
+			template<typename T>
+			using submap = std::map<Key, std::unique_ptr<T> >;
+			
+			static type_id type_id_counter(bool increment){
+				static type_id counter = 0;
+				if (increment) ++counter;
+				return counter;
+			}
+
+			template<typename T>
+			static type_id get_type_id(){
+				static auto id_for_T = type_id_counter(true);
+				return id_for_T;
+			}
+
+			std::map<type_id, std::pair<void*, destructor> > sub_maps;
+			std::set<Key> member_set; //for membership queries, when we do not know the type
+
+			template<typename T>
+			auto* get_submap(){
+				auto tid = get_type_id<T>();
+				if (sub_maps.count(tid) == 0){
+					submap<T>* newmap = new submap<T>{};
+					sub_maps.emplace(
+						tid,
+						std::pair<void*,destructor>(newmap,[newmap](){delete newmap;}));
+				}
+				return (submap<T>*) sub_maps[tid].first;
+			}
+
+			template<typename T>
+			auto get_submap() const {
+				return (submap<T>*) sub_maps.at(get_type_id<T>()).first;
+			}
+		public:
+
+			template<typename T>
+			auto& at(Key i) const {
+				auto tid = get_type_id<T>();
+				assert(sub_maps.count(tid) > 0);
+				return get_submap<T>()->at(i);
+			}
+
+			template<typename T>
+			auto& mut(Key i){
+				member_set.insert(i);
+				return (*get_submap<T>())[i];
+			}
+
+			bool contains(Key i){
+				return member_set.count(i) > 0;
+			}
+
+			virtual ~HeteroMap(){
+				for (auto& p : sub_maps){
+					p.second.second();
+				}
+			}
+		};
+	}
+}
