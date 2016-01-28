e88e3f135510d09a13db3260b78f2daaa5307e0a
Matthew Milano <milano@cs.cornell.edu>
2016-01-28 01:11:52 -0500

    choice is not broken.  fix for run_strong bug.

--- transactions/mtl/Operate_macros.hpp
+++ transactions/mtl/Operate_macros.hpp
@@ -76,9 +76,9 @@
 			}															\
 																		\
 			void strongCall(mtl::TransactionContext* ctx CMA StrongCache& c CMA const StrongStore &s CMA std::false_type*) const { \
+				run_strong_helper(ctx,c,s,argcnt(n));					\
 			}															\
 			auto strongCall(mtl::TransactionContext* ctx CMA StrongCache& c CMA  const StrongStore &s) const { \
-				run_strong_helper(ctx,c,s,argcnt(n));					\
 				choose_strong<get_level<OperateImpl>::value> choice{nullptr}; \
 				return strongCall(ctx,c,s,choice);						\
 			}															\
