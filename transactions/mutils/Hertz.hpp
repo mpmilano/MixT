3493a0f46523edd2a94ce2fb10ab49032260463a
Matthew Milano <milano@cs.cornell.edu>
2016-02-24 16:53:49 -0500

    try not to forget a file next time

---
+++ transactions/mutils/Hertz.hpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+namespace mutils{
+
+	struct Frequency
+	{
+		void Print() const { std::cout << hertz << "Hz\n"; }
+		const unsigned long long hertz;
+		constexpr auto operator+(const Frequency& fr) const {
+			return Frequency{fr.hertz + hertz};
+		}
+		constexpr auto operator*(const Frequency& fr) const {
+			return Frequency{fr.hertz * hertz};
+		}
+	};
+	constexpr Frequency operator"" _Hz(unsigned long long hz)
+	{
+		return Frequency{hz};
+	}
+	constexpr Frequency operator"" _kHz(long double khz)
+	{
+		unsigned long long coerce = khz * 1000;
+		return Frequency{coerce};
+	}
+
+	constexpr Frequency as_hertz(unsigned long long hz){
+		return Frequency{hz};
+	}
+
+}
