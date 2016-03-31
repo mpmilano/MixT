63bf8160f3bf8aa82f3add643c568e23c956c433
Matthew Milano <milano@cs.cornell.edu>
2016-03-31 17:47:44 -0400

    we actually do need to rebuild vm_main every time, sorry.

--- euc/as-gentoo.sh
+++ euc/as-gentoo.sh
@@ -6,7 +6,10 @@ git pull
 source pg_env.sh
 killall -9 vm
 rm /tmp/Myria*
-if [[ $6 ]]; then make clean; fi
+if [[ $6 ]];
+then make clean
+else rm vm_main.o; rm vm
+fi
 concurrencySetting="$2" defineThis="$3" causalGroup="$1" MY_IP="$4" STRONG_REMOTE_IP="$5" make -j4 vm
 ./vm
 #for ((i=1; i <= $2; i++))
