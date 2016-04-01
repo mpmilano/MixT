707df3abc403e49c6751784310566ef757c5e56a
Matthew Milano <milano@cs.cornell.edu>
2016-03-31 21:25:18 -0400

    moving utils into separate repos

--- transactions/mutils/restrict.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#define restrict(x) typename ignore = typename std::enable_if<(x)>::type
-#define restrict2(x) typename ignore = void, typename ignore2 = typename std::enable_if<(x)>::type
-
-#define CMA ,
