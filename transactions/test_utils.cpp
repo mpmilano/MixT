daa0ff4916f55d9199a3076f75f59f4860589544
Matthew Milano <milano@cs.cornell.edu>
2016-06-26 01:15:59 -0400

    missing file

---
+++ transactions/test_utils.cpp
@@ -0,0 +1,43 @@
+#include "test_utils.hpp"
+
+namespace mutils{
+
+//I'm guessing miliseconds.  Here's hoping!
+	milliseconds getArrivalInterval(Frequency arrival_rate) {
+		using namespace std;
+		using namespace chrono;
+		// exponential
+		constexpr double thousand = -1000.0;
+		double U = better_rand();
+		double T = thousand * log(U) / arrival_rate.hertz;
+		unsigned long l = round(T);
+		return milliseconds(l);
+	}
+
+	unsigned int get_zipfian_value(unsigned int max, double alpha){
+		double y = better_rand();
+		assert (y < 1.1);
+		assert (y > -0.1);
+		double max_pow = pow(max,1 - alpha);
+		double x = pow(max_pow*y,1/(1-alpha));
+		return round(x);
+	}
+
+	const time_point launch_clock = std::chrono::high_resolution_clock::now();
+	
+//was as microseconds
+	duration elapsed_time() {
+		return std::chrono::high_resolution_clock::now() - launch_clock;
+	};
+
+	
+	/*inline int get_strong_ip() {
+		static int ip_addr{[](){
+		std::string static_addr {STRONG_REMOTE_IP};
+				if (static_addr.length() == 0) static_addr = "127.0.0.1";
+				return mutils::decode_ip(static_addr);
+			}()};
+		return ip_addr;
+		}*/
+
+}
