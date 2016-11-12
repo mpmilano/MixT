ee9f5a4b49bbd06d0c8a5c369975574554b6d1af
Matthew Milano <milano@cs.cornell.edu>
2016-11-11 19:51:44 -0500

    remove legacy checks

--- transactions/pgsql/SQLTransaction.hpp
+++ transactions/pgsql/SQLTransaction.hpp
@@ -52,21 +52,9 @@ namespace myria{
 				sql_conn->conn->receive(t...);
 				whendebug(log_receive_stop(what);)
 			}
-
-			void check_serialization_failure()  {
-				char failed{-1};
-				receive(level_string + " check_failure", failed);
-				assert(failed != -1);
-				assert(failed == 1 || failed == 0);
-				if (failed == 1){
-					remote_aborted = true;
-					throw SerializationFailure();
-				}
-			}
 			
 			bool exists(Name n){
 				prepared(level_string + " exists",TransactionNames::exists,n);
-				check_serialization_failure();
 				bool b{false};
 				receive("exists", b);
 				return b;
@@ -74,20 +62,17 @@ namespace myria{
 
 			void Del(Name n){
 				prepared("del",TransactionNames::Del,n);
-				check_serialization_failure();
 			}
 			
 			template<typename Vers>
 			void select_version(Table t, Name n, Vers& vers){
 				prepared(level_string + " select version", TransactionNames::select_version, t,n);
-				check_serialization_failure();
 				receive(level_string + " select version",vers);
 			}
 
 			template<typename Vers>
 			std::vector<char> select_version_data(Table t, Name n, Vers& vers){
 				prepared(level_string + " selct_version_data",TransactionNames::select_version_data,t,n);
-				check_serialization_failure();
 				receive(level_string + " select_version_data",vers);
 				std::vector<char> v;
 				if (t == Table::BlobStore) {
@@ -109,7 +94,6 @@ namespace myria{
 			void update_data(Table t, Name n, Data &d, Vers& vers){
 				assert(gstore.level == Level::strong);
 				prepared("update_data, strong",TransactionNames::update_data, t,n,d);
-				check_serialization_failure();
 				receive("update_data version, strong", vers);
 			}
 
@@ -117,7 +101,6 @@ namespace myria{
 			void update_data(Table t, const RG& rg, Name n, const Clock& c, Data &d, Vers& vers){
 				assert(gstore.level == Level::causal);
 				prepared("update_data, causal",TransactionNames::update_data,t,rg,n,c,d);
-				check_serialization_failure();
 				receive("update_data version, causal", vers);
 			}
 
@@ -125,21 +108,18 @@ namespace myria{
 			void initialize_with_id(Table t, Name id, const Blob &b) {
 				assert(gstore.level == Level::strong);
 				prepared("initialize_with_id, strong",TransactionNames::initialize_with_id,t,id,b);
-				check_serialization_failure();
 			}
 			
 			template<typename RG, typename Clock, typename Blob>
 			void initialize_with_id(Table t, const RG& rg, Name id, const Clock& c, const Blob &b){
 				assert(gstore.level == Level::causal);
 				prepared("initialize_with_id, causal",TransactionNames::initialize_with_id,t,rg,id,c,b);
-				check_serialization_failure();
 			}
 
 			template<typename Vers>
 			void increment(Name n, Vers& vers){
 				assert(gstore.level == Level::strong);
 				prepared("increment, strong",TransactionNames::increment,n);
-				check_serialization_failure();
 				receive("increment, strong", vers);
 			}
 
@@ -147,7 +127,6 @@ namespace myria{
 			void increment(const RG& rg, Name n, const Clock& c, Vers& vers){
 				assert(gstore.level == Level::causal);
 				prepared("increment, causal",TransactionNames::increment,rg,n,c);
-				check_serialization_failure();
 				receive("increment, causal", vers);
 			}
 
@@ -156,7 +135,6 @@ namespace myria{
 				char trans{0};
 				log_send(level_string + " commit");
 				sql_conn->conn->send(trans);
-				check_serialization_failure();
 				return true;
 			}
 
