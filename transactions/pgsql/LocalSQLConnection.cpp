a45cdf9e7108db554a05959785dd49394de91ec8
Matthew Milano <milano@cs.cornell.edu>
2016-11-12 16:23:54 -0500

    lots of debugging later; error uncovered; remember to submit new transactions for processing *after* consuming input from old ones

--- transactions/pgsql/LocalSQLConnection.cpp
+++ transactions/pgsql/LocalSQLConnection.cpp
@@ -3,6 +3,11 @@
 #include "pgexceptions.hpp"
 #include "pgresult.hpp"
 #include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 using namespace mutils;
 
@@ -29,29 +34,66 @@ namespace myria { namespace pgsql {
 			namespace {
 				template<typename transaction_list>
 				void clear_completed_transactions(transaction_list &transactions){
-					while (true){
-						auto &front = transactions.front();
-						if (front.actions.size() == 0 && front.no_future_actions()) {
-							transactions.pop_front();
-						}
+					if (transactions.size() > 0)
+						while (true){
+							auto &front = transactions.front();
+							if (front.actions.size() == 0 && front.no_future_actions()) {
+								transactions.pop_front();
+							}
 						else break;
-					}
+						}
 				}
 			}
 
+#ifndef NDEBUG
+			namespace {
+				bool select_indicates_data_avilable(int fd){
+					fd_set rfds;
+					struct timeval tv;
+					int retval;
+
+					/* Watch stdin (fd 0) to see when it has input. */
+
+					FD_ZERO(&rfds);
+					FD_SET(fd, &rfds);
+
+					/* Wait up to one second. */
+
+					tv.tv_sec = 1;
+					tv.tv_usec = 0;
+
+					retval = select(fd + 1, &rfds, NULL, NULL, &tv);
+					/* Don't rely on the value of tv now! */
+
+					if (retval == -1)
+						perror("select()");
+					else return retval;
+					assert(false && "select error");
+				}
+			}
+#endif
 			void LocalSQLConnection_super::tick(){
+#ifndef NDEBUG
+				if (select_indicates_data_avilable(underlying_fd()))
+					std::cout << "======SELECT indicates data available" << std::endl;
+#endif
 				clear_completed_transactions(transactions);
-				try {
-					if (transactions.size() > 0){
-						auto &front = transactions.front();
-						if (front.actions.size() > 0){
-							auto &action = front.actions.front();
-							if (!action.submitted){
-								action.submitted = true;
-								action.query();
+
+				auto submit_new_transaction = [&]{
+							if (transactions.size() > 0){
+								auto &front = transactions.front();
+								if (front.actions.size() > 0){
+									auto &action = front.actions.front();
+									if (!action.submitted){
+										action.submitted = true;
+										action.query();
+									}
+								}
 							}
-						}
-					}
+						};
+				
+				try {
+					submit_new_transaction();
 					PQconsumeInput(conn);
 					while (!PQisBusy(conn)){
 						if (auto* res = PQgetResult(conn)){
@@ -66,18 +108,25 @@ namespace myria { namespace pgsql {
 						}
 						else break;
 					}
+					//whendebug(std::cout << "PQ tick successfully completed!" << std::endl);
 				}
 				catch (SerializationFailure& sf){
+					whendebug(std::cerr << "serialization failure detected!" << std::endl);
 					transactions.front().indicate_no_future_actions();
 					transactions.pop_front();
 					throw sf;
 				}
+				clear_completed_transactions(transactions);
+				submit_new_transaction();
 			}
 			
 			int LocalSQLConnection_super::underlying_fd() {
-				return PQsocket(conn);
+				auto fd = PQsocket(conn);
+				assert(fd >= 0);
+				return fd;
 			}
 			LocalSQLConnection_super::~LocalSQLConnection_super(){
+				whendebug(std::cout << "closing a postgres connection!" << std::endl;)
 				PQfinish(conn);
 			}
 			
--- transactions/pgsql/basic_sql_test.cpp
+++ transactions/pgsql/basic_sql_test.cpp
@@ -1,7 +1,7 @@
 #include "LocalSQLConnection.hpp"
-#include "BlobUtils.hpp"
 #include "pgtransaction.hpp"
 #include "pgexceptions.hpp"
+#include "epoll.hpp"
 #include <pqxx/pqxx>
 using namespace myria;
 using namespace pgsql;
@@ -14,12 +14,13 @@ int main(){
 		pqxx::connection c;
 		c.prepare("pqxx","update \"BlobStore\".\"BlobStore\" set data = $1 where id = 1;");
 		pqxx::work trans{c};
-		auto bytes = make_blob(Bytes{(char*) &random_stuff[0] ,sizeof(unsigned long long)*4});
+		auto bytes = pqxx::binarystring{(char*) &random_stuff[0] ,sizeof(unsigned long long)*4};
 		trans.prepared("pqxx")(bytes).exec();
 		trans.commit();
 	}
 		try {
-			LocalSQLConnection_super c;
+			auto _c = std::make_unique<LocalSQLConnection_super>();
+			auto& c = *_c;
 			using transaction = pgtransaction;
 			const std::string name = "test_statement";
 			{
@@ -52,13 +53,24 @@ int main(){
 					std::cout << "YAY byte order is maintained for blobs.  praise the lord." << std::endl;
 				},"select data from \"BlobStore\" where id = 1");
 			trans.commit([](){
-					std::cout << "we committed" << std::endl;
+					std::cout << "we committed; ALL PQ COMMANDS COMPLETE" << std::endl;
 				});
 			std::cout << "commit transaction 2" << std::endl;
-			for (int i = 0; i < 10000; ++i){
+			EPoll ep;
+			std::function<void (EPoll&, LocalSQLConnection_super&)> f =
+				[](EPoll&, LocalSQLConnection_super& c){
+				//std::cout << "we ticked" << std::endl;
 				c.tick();
-			}
-			std::cout << "normal termination" << std::endl;
+			};
+			std::cout << "waiting on server" << std::endl;
+			ep.add(std::move(_c),f);
+			c.tick();
+			std::cout << "manual tick complete" << std::endl;
+			//for (int i = 0; i < 10000; ++i) c.tick();
+			for (int i = 0; i < 10000; ++i)
+				ep.wait();
+			std::cout << "fell through ep" << std::endl;
+			sleep(4000);
 		}
 		catch(const std::exception& e){
 			std::cout << e.what() << std::endl;
