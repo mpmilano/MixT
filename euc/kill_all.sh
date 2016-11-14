c2bb34c2edcea79c8729b99ac6c9d4f7297accd4
Matthew Milano <milano@cs.cornell.edu>
2016-11-14 01:49:21 -0500

    script changes

---
+++ euc/kill_all.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [[ -z "$instance_list" ]]
+then
+	echo "failure: specify instance_list as environment variable"
+	exit 1
+fi
+
+for foo in $instance_list; do
+	ssh -o "UserKnownHostsFile /dev/null"  -o strictHostKeyChecking=no -i MyriaInstances.pem ubuntu@$foo sudo killall -9 screen
+done
