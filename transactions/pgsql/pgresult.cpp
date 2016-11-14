fa89b6fbab37ab2379f176562f40dd93ba281ad3
Matthew Milano <milano@cs.cornell.edu>
2016-11-14 01:05:50 -0500

    more debug

--- transactions/pgsql/pgresult.cpp
+++ transactions/pgsql/pgresult.cpp
@@ -43,7 +43,8 @@ namespace myria { namespace pgsql {
 				else if (status == PGRES_BAD_RESPONSE
 						 || status == PGRES_FATAL_ERROR
 						 || status == PGRES_NONFATAL_ERROR){
-					assert(false);
+					std::cerr <<"byebye" << std::endl;
+					exit(-1);
 					if (auto _errorcode = PQresultErrorField(res,PG_DIAG_SQLSTATE)){
 						std::string errorcode{_errorcode};
 						if (errorcode == "40000"
