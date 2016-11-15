1e102bf072a066b491f25c5879e37006205b1663
Matthew Milano <milano@cs.cornell.edu>
2016-11-14 21:58:59 -0500

    script fix

--- euc/analyzers/how_many_aborts.py
+++ euc/analyzers/how_many_aborts.py
@@ -3,13 +3,14 @@
 import quick_analyzer
 from bin_things import bin_things
 
-abort_list = [(a,b) for (a,b) in quick_analyzer.stream_operator(lambda log : (log.is_serialization_error, log.remote_failure_string)) if a]
+full_list = quick_analyzer.stream_operator(lambda log : log.remote_failure_string)
+abort_list = [a for a in full_list if len(a) > 2 ]
 print('number aborts: ')
 print(len(abort_list))
 print('number commits ')
-print(len([x for x in quick_analyzer.stream_operator(lambda log : log.is_serialization_error) if not x]))
+print(len(full_list) - len(abort_list))
 print('abort messages: ')
-for (ignore,message) in abort_list:
+for message in abort_list:
     print(message)
 
 
