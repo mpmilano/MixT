c6f06f68cdf96371dd00cd049d82ef169ff92cf3
Matthew Milano <milano@cs.cornell.edu>
2017-04-05 14:27:18 -0400

    turn back on remote running, crank up test

--- transactions/vm_main.cpp
+++ transactions/vm_main.cpp
@@ -102,7 +102,7 @@ namespace synth_test {
 	void perform_increment(unique_ptr<VMObjectLog>& log_messages,
 												 DeserializationManager* dsm, mutils::connection& conn, Tracker &, Hndl hndl){
 		constexpr auto trans = TRANSACTION(Hndl::label::int_id::value,let remote x = hndl in {x = x + 1})::WITH(hndl);
-		trans.run_local(dsm,conn,hndl);
+		trans.run_optimistic(dsm,conn,hndl);
 			log_messages->addField(
 				LogFields::is_write,true);
 	}
@@ -111,7 +111,7 @@ namespace synth_test {
 	void perform_read(unique_ptr<VMObjectLog>& log_messages,
 										DeserializationManager* dsm, mutils::connection& conn, Tracker &, Hndl hndl){
 		constexpr auto trans = TRANSACTION(150 + Hndl::label::int_id::value,let remote x = hndl in {})::WITH(hndl);
-		trans.run_local(dsm,conn,hndl);
+		trans.run_optimistic(dsm,conn,hndl);
 
 #ifndef NDEBUG
 		struct tmptest{ int a;};
