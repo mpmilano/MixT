74fc8e821b230d35d3b880b8dcd6c415b6d21be4
Matthew Milano <milano@cs.cornell.edu>
2017-04-08 19:56:33 -0400

    tombstone mostly revert

--- transactions/tracker/Tombstone.hpp
+++ transactions/tracker/Tombstone.hpp
@@ -5,16 +5,11 @@ namespace myria { namespace tracker {
 		using Nonce = int;
 		
     struct Tombstone {
-      const Nonce nonce;
-      const int ip_addr;
-      const int portno;
+			Nonce nonce;
+			int ip_addr;
+      int portno;
       Name name() const;
-      
-      /*
-	int to_bytes(char* v) const;
-	int bytes_size() const;
-	static std::unique_ptr<Tombstone> from_bytes(DeserializationManager*,char const* v);
-	Tombstone(Nonce n,int ip, int portno):nonce(n),ip_addr(ip),portno(portno){}
-	Tombstone(const Tombstone& t):nonce(t.nonce),ip_addr(t.ip_addr),portno(t.portno){} //*/
+
     };
+		static_assert(std::is_pod<Tombstone>::value);
   }}
