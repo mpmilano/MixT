6621d225fc5f6476059e1c4bc1fb892c7b1878f0
Matthew Milano <milano@cs.cornell.edu>
2017-04-13 14:06:47 -0400

    better analyzer script, turn tracking back on

--- euc/analyzers/estimate_max_throughput.sh
+++ euc/analyzers/estimate_max_throughput.sh
@@ -8,8 +8,9 @@ lines=`wc -l $file | awk '{print $1}'`
 echo $lines
 echo $granularity
 iters=$[lines / granularity]
-cat $file | sort -n > "$file"-sorted
-for ((i=$[1 + 0$offset]; i < $iters; i=$i+1)); do
+uselines=$[offset * granularity]
+cat  $file | tail -$uselines | sort -n > "$file"-sorted
+for ((i=0; i < $offset; i=$i+1)); do
 		start=`head -$[$granularity + ($i * $granularity)] "$file"-sorted | tail -$granularity | head -1 | awk '{print $1}' | tr -d ','`
 		end=`head -$[$granularity + ($i * $granularity)] "$file"-sorted | tail -$granularity | tail -1 | awk '{print $2}' | tr -d ','`
 		echo $[ ($granularity * 1000000) / (end-start)]
