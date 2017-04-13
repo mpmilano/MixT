bf6250635267088135bf615a555801873f52ec81
Matthew Milano <milano@cs.cornell.edu>
2017-04-13 08:33:45 -0400

    quick max throughput estimates

---
+++ euc/analyzers/estimate_max_throughput.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+file=$1
+
+granularity=1000
+lines=`wc -l $file | awk '{print $1}'`
+echo $lines
+echo $granularity
+iters=$[lines / granularity]
+cat $file | sort -n > "$file"-sorted
+for ((i=1; i < $iters; i=$i+1)); do
+		start=`head -$[$granularity + ($i * $granularity)] "$file"-sorted | tail -$granularity | head -1 | awk '{print $1}' | tr -d ','`
+		end=`head -$[$granularity + ($i * $granularity)] "$file"-sorted | tail -$granularity | tail -1 | awk '{print $2}' | tr -d ','`
+		echo $[ ($granularity * 1000000) / (end-start)]
+done
