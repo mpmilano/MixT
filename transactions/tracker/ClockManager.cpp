b2d71b84470235b40afeadb8314d9d88fbe70fab
Matthew Milano <milano@cs.cornell.edu>
2017-04-19 02:14:05 -0400

    wip; tracker keeps track of recent timestamps

--- transactions/tracker/ClockManager.cpp
+++ transactions/tracker/ClockManager.cpp
@@ -63,6 +63,11 @@ void Tracker::updateClock() {
   }
   assert(ends::prec(i->global_min, newc));
   i->global_min = newc;
+	std::vector<Clock> newest_objects;
+	for (const auto& c : i->newest_objects){
+		if (!ends::prec(c,newc)) newest_objects.push_back(c);
+	}
+	i->newest_objects = std::move(newest_objects);
 }
 }
 }
