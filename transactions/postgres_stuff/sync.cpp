34f853d7ba56a609eb1d7988e8cf82905ab5f053
Matthew Milano <milano@cs.cornell.edu>
2017-04-23 18:31:42 -0400

    turning back on clock sync for the first time in almost a year

--- transactions/postgres_stuff/sync.cpp
+++ transactions/postgres_stuff/sync.cpp
@@ -9,33 +9,13 @@ int main(){
 	
 	std::vector<pair<int,shared_ptr<Socket> > > ip_addrs {
 		{
-make_pair(decode_ip("128.84.105.158"), std::shared_ptr<Socket>()),
-make_pair(decode_ip("128.84.105.107"), std::shared_ptr<Socket>()),
-make_pair(decode_ip("128.84.105.103"), std::shared_ptr<Socket>()),
-make_pair(decode_ip("128.84.105.148"), std::shared_ptr<Socket>()),
-make_pair(decode_ip("128.84.105.88"), std::shared_ptr<Socket>()),
-make_pair(decode_ip("128.84.105.72"), std::shared_ptr<Socket>()),
-make_pair(decode_ip("128.84.105.70"), std::shared_ptr<Socket>()),
-make_pair(decode_ip("128.84.105.118"), std::shared_ptr<Socket>()),
-make_pair(decode_ip("128.84.105.92"), std::shared_ptr<Socket>()),
-make_pair(decode_ip("128.84.105.140"), std::shared_ptr<Socket>()),
-make_pair(decode_ip("128.84.105.128"), std::shared_ptr<Socket>()),
-make_pair(decode_ip("128.84.105.81"), std::shared_ptr<Socket>()),
-make_pair(decode_ip("128.84.105.77"), std::shared_ptr<Socket>()),
-make_pair(decode_ip("128.84.105.98"), std::shared_ptr<Socket>()),
-make_pair(decode_ip("128.84.105.153"), std::shared_ptr<Socket>()),
-make_pair(decode_ip("128.84.105.90"), std::shared_ptr<Socket>()),
-make_pair(decode_ip("128.84.105.122"), std::shared_ptr<Socket>()),
-make_pair(decode_ip("128.84.105.121"), std::shared_ptr<Socket>()),
-make_pair(decode_ip("128.84.105.66"), std::shared_ptr<Socket>()),
-make_pair(decode_ip("128.84.105.135"), std::shared_ptr<Socket>())
-
+make_pair(decode_ip("128.253.3.197"), std::shared_ptr<Socket>()),
+make_pair(decode_ip("128.84.217.139"), std::shared_ptr<Socket>()),
 					}};
 
 	try { 
 
 		auto conn_causal = init_causal();
-		auto conn_strong = init_strong();
 
 		std::cout << "beginning loop" << std::endl;
 		while (true){
--- transactions/postgres_stuff/sync_defs.h
+++ transactions/postgres_stuff/sync_defs.h
@@ -13,16 +13,9 @@ const string id = to_string(
 #include "../big_prime"
 	);
 
-auto init_strong(){
-	auto conn_strong = make_unique<pqxx::connection>("host=128.84.217.149");
-	conn_strong->prepare("Update",
-						string("update \"BlobStore\" set data=$1,version=$2 where id= ") + id);
-	return conn_strong;
-}
-
 auto init_causal(){
 	auto conn_causal = make_unique<pqxx::connection>();
-	conn_causal->prepare("selectit","select max(vc1) as vc1,max(vc2) as vc2,max(vc3) as vc3,max(vc4) as vc4 from (select max(vc1) as vc1,max(vc2) as vc2,max(vc3) as vc3,max(vc4) as vc4 from \"BlobStore\" union select max(vc1) as vc1,max(vc2) as vc2,max(vc3) as vc3,max(vc4) as vc4 from \"IntStore\") as foo");
+	conn_causal->prepare("selectit","select a,a,a,a from (SELECT (extract(epoch from current_timestamp)*100000)::bigint as a) as t;");
 	return conn_causal;
 }
 
@@ -33,34 +26,4 @@ void select_causal_timestamp(pqxx::connection& conn_causal, std::array<long long
         bool assrt = r[0][0].to(tmp[0]) && r[0][1].to(tmp[1]) && r[0][2].to(tmp[2]) && r[0][3].to(tmp[3]);
         assert(assrt);
 	trans_causal.commit();
-}
-
-void update_strong_clock(pqxx::connection& conn_strong, std::array<long long,4>& tmp){
-	pqxx::work trans_strong(conn_strong);
-	trans_strong.exec("set search_path to \"BlobStore\",public");
-	binarystring blob(&tmp[0],tmp.size()*4);
-	auto r = trans_strong.prepared("Update")(blob)(tmp[0]+tmp[1]+tmp[2]+tmp[3]).exec();
-	assert(r.affected_rows() > 0);
-	trans_strong.commit();
-}
-
-void select_strong_clock(pqxx::connection& conn_strong, std::array<long long,4>& arr){
-	pqxx::work trans_strong(conn_strong);
-	trans_strong.exec("set search_path to \"BlobStore\",public");
-	auto r = trans_strong.exec
-		(string("select data from \"BlobStore\" where id = ") + id);
-	binarystring bs
-		(r[0][0]);
-	//for (int i : arr) std::cout << i << " ";
-				//std::cout << std::endl;
-	memcpy(&arr[0],bs.data(),arr.size()*4);
-}
-
-void verify_strong_clock(pqxx::connection& conn_strong, std::array<long long,4>& tmp){
-	std::array<long long,4> arr;
-	select_strong_clock(conn_strong, arr);
-	assert(arr[0] == tmp[0]);
-	assert(arr[1] == tmp[1]);
-	assert(arr[2] == tmp[2]);
-	assert(arr[3] == tmp[3]);
 }
