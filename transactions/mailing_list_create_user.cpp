c2b494ca49bc47a0261e6f7393732a947570678f
Matthew Milano <milano@cs.cornell.edu>
2017-09-17 23:21:11 +0000

    mailing list transactions in pre-compiled form; no longer need we fear the make clean

--- transactions/mailing_list_create_user.cpp
+++ transactions/mailing_list_create_user.cpp
@@ -2,6 +2,11 @@
 
 namespace examples {
 		user_hndl create_user(ClientTrk& ct, groups& g){
+#ifdef USE_PRECOMPILED
+			constexpr
+#include "mailing_list_create_user.cpp.precompiled"
+				txn;
+#else
 		constexpr auto txn = TRANSACTION(
 			var sample_user_hndl = (g->value).users->value.ensure(causal),
 			var new_user = (*sample_user_hndl).ensure(causal),
@@ -9,6 +14,7 @@ namespace examples {
 			new_user.i->next = new_user.i->next.nulled().ensure(causal),
 			return sample_user_hndl.new(new_user);
 			)::WITH(g);
+#endif
 		return txn.run_local(ct,g);
 	}
 }
