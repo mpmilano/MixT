86985af927daa4badfca16b667d342642f5d823b
Matthew Milano <milano@cs.cornell.edu>
2017-10-23 20:47:44 -0400

    bugfixes

--- transactions/pgsql/mailing_list_loop_causal.sh
+++ transactions/pgsql/mailing_list_loop_causal.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo 'delete from causalstore."BlobStore";' | psql -U research -d DataStore
-echo 'insert into causalstore."BlobStore" select * from causalstore."MailingListExperimentBlobStore;' | psql -U research -d DataStore
+echo 'insert into causalstore."BlobStore" select * from causalstore."MailingListExperimentBlobStore";' | psql -U research -d DataStore
 ../../euc/vacuum.sh localhost
 ./causal_relay 8877
